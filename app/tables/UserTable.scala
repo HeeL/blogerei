44726d3e60b2ef7131423ec3ec379fb095f4c596
Sergii Paryzhskyi <parizhskiy@gmail.com>
2015-07-13 07:45:20 +0200

    defined a table for users

---
+++ app/tables/UserTable.scala
@@ -0,0 +1,24 @@
+package tables
+
+import models.User
+import slick.driver.PostgresDriver.api._
+import slick.driver._
+
+trait UserTable {
+
+  class Users(tag: Tag) extends Table[User](tag, "users") {
+
+    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+
+    def email = column[String]("email")
+
+    def password = column[String]("password")
+
+    def * = (id, email, password) <> ((User.apply _).tupled, User.unapply)
+  }
+
+  val users = TableQuery[Users]
+
+  def findUser(email: String, password: String) = users.filter(_.email === email && _.password === password)
+
+}
