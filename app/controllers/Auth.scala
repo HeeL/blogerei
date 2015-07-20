9a2e31f830e35f29896dcd30b499935511e09e42
Sergii Paryzhskyi <parizhskiy@gmail.com>
2015-07-20 21:34:14 +0200

    refactored auth controller by moving queries to UserTable

--- app/controllers/Auth.scala
+++ app/controllers/Auth.scala
@@ -6,22 +6,15 @@ import play.api._
 import play.api.data._
 import play.api.data.Forms._
 import play.api.mvc._
-import play.api.Play.current
-import play.api.db.slick.DatabaseConfigProvider
-import play.api.db.slick.HasDatabaseConfig
-import slick.driver.JdbcProfile
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
 
-class Auth extends Controller with UserTable with HasDatabaseConfig[JdbcProfile] {
-  val dbConfig = DatabaseConfigProvider.get[JdbcProfile](Play.current)
+class Auth extends Controller with UserTable {
 
   lazy val authForm = Form(
     tuple(
       "email" -> nonEmptyText,
       "password" -> nonEmptyText) verifying ("Invalid user or password", result => result match {
       case (email, password) => {
-        Await.result(db.run(authenticate(email, password)), Duration.Inf) == 1
+        authenticate(email, password)
       }
       case _ => false
     })
--- app/tables/UserTable.scala
+++ app/tables/UserTable.scala
@@ -1,10 +1,20 @@
 package tables
 
 import models.User
+import scala.concurrent.Future
+import play.api.Play
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.db.slick.HasDatabaseConfig
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import slick.driver.JdbcProfile
+import slick.lifted.Tag
 import slick.driver.PostgresDriver.api._
-import slick.driver._
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
-trait UserTable {
+
+trait UserTable extends HasDatabaseConfig[JdbcProfile]{
+  val dbConfig = DatabaseConfigProvider.get[JdbcProfile](Play.current)
 
   class Users(tag: Tag) extends Table[User](tag, "users") {
 
@@ -19,8 +29,12 @@ trait UserTable {
 
   val users = TableQuery[Users]
 
+  def getByEmailAndPassword(email: String, password: String) = {
+    users.filter(u => u.email === email && u.password === password)
+  }
+
   def authenticate(email: String, password: String) = {
-    users.filter(u => u.email === email && u.password === password).length.result
+    Await.result(db.run(getByEmailAndPassword(email, password).length.result), Duration.Inf) == 1
   }
 
 }
